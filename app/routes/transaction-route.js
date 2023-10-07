d7232b4da0751a17982edef95d70ff756b58286e
labkumardas <labdas201@gmail.com>
2023-10-07 11:54:39 +0530

    update code

--- app/routes/transaction-route.js
+++ app/routes/transaction-route.js
@@ -1,5 +1,9 @@
 //
 //
+const transactionController = require('../controller/transaction/transactionController');
+
 const isAuthorized = require('../middleware/isAuthorized');
 const upload = require('../middleware/multer');
-module.exports = function (app) {};
+module.exports = function (app) {
+  app.get(`/transaction/list`, isAuthorized, transactionController.index);
+};
